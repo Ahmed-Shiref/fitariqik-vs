8ee7bd4ad99b93ecc6422a371e1c922847a29cbb
Ahmed-Shiref <Ahmed-Shiref@gmail.com>
2024-05-08 10:59:20 +0300

    adding the project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fitariqik.Startup))]
+namespace fitariqik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
